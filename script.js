5bb44102acd427afe8ae8e6ba528a0514103185d
LegendFabian4 <121681426+LegendFabian4@users.noreply.github.com>
2024-02-07 11:44:55 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function button11() {
+    try {
+    alert("click OK to redirect")
+    console.log("redirecting...")
+    window.open("index1.html")
+    console.log("done!")
+    }
+    catch {
+        console.error("can't redirect")
+    }
+}
